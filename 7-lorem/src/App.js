17e817fd3275f1ae7fd2fb4330c0694dc8317591
karannya <annyakar.91@gmail.com>
2023-02-06 11:58:56 +0200

    final commit

--- 7-lorem/src/App.js
+++ 7-lorem/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import text from './Data'
 
 function App() {
+  const [count,setCount]=useState(0)
+  const [data,setData]=useState([])
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    let amount = parseInt(count);
+         if(count <=0){
+          amount = 1;
+         }
+         else if (count > 8) {
+          amount = 8;
+        }
+        setData(text.slice(0, amount));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='mainDiv'>
+     <h1>Tired of boring Lorem ipsum?</h1>
+     <div className='formDiv'>
+     <form onSubmit={handleSubmit}>
+     <label> Paragraph: </label>
+     <input  className='insertCount' htmlFor='amount' type='number'
+            name='amount'
+            value={count}
+            id='amount'
+            onChange={(e) => setCount(e.target.value)}
+            />
+            <button className='btn'>GENERATE</button>
+            
+     </form>
+     </div>
+     <div className='textDiv'>
+     {data.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+     </div>
     </div>
+    </>
   );
 }
 
