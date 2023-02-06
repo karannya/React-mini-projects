9533d41b664020d3b9136af9d1d67811faf0f9f0
karannya <annyakar.91@gmail.com>
2023-02-06 12:03:48 +0200

    final commit

--- 8-grocery-note/src/App.js
+++ 8-grocery-note/src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState,useEffect} from 'react'
+import { FaTrash,FaEdit } from 'react-icons/fa'
+import Notification from './Notification'; 
 function App() {
+
+  const getLocalStorage = () => {
+    let data = localStorage.getItem('data');
+    if (data) {
+      return JSON.parse(data)
+    } else {
+      return [];
+    }
+  };
+  const [data,setData]=useState(getLocalStorage());
+  const [updatedata,setUpdateData]=useState('')
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  
+  const getData=(e)=>{
+    
+    setUpdateData(e.target.value)
+   
+  }
+  const addData=()=>{
+    
+   
+    if(!updatedata){
+      setErrorMessage({
+        text: 'Please enter the data ',
+        type: 'error'
+      })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+   
+    }
+    else if(updatedata && isEditing){
+      setData(
+        data.map((item) => {
+          if (item.id === editID) {
+            return { ...item, content: updatedata };
+          }
+          return item;
+        })
+      );
+      setUpdateData('');
+      setEditID(null);
+      setIsEditing(false);
+      setErrorMessage({
+        text: 'data updated successfully',
+        type: 'success'
+      })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+    }
+    else{
+      
+      setErrorMessage({
+        text: 'Data entered successfully ',
+        type: 'success'
+      })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+      const newObject={
+        content: updatedata,
+        id: data.length + 1
+      }
+      setData([...data,newObject])
+      setUpdateData('')
+    }
+   
+    
+  }
+  const editItem=(id)=>{
+    let afterEdit=data.find((ele)=>{
+      return ele.id===id
+    })
+    setUpdateData(afterEdit.content)
+    setIsEditing(true);
+    setEditID(id)
+   
+    
+  }
+  const removeItem=(id)=>{
+    let afterDelete=data.filter((ele)=>{
+      return id!==ele.id
+    })
+      setData(afterDelete)
+      setErrorMessage({
+        text: 'Data deleted successfully ',
+        type: 'error'
+      })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+  }
+  const clearList=()=>{
+    setData([])
+    setErrorMessage({
+      text: 'All the data cleared ',
+      type: 'error'
+    })
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 2000)
+  }
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data));
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   
+    <div className="mainDiv">
+    <Notification message={errorMessage}/>
+    <h2>grocery bud</h2> 
+    <span className='enterData'>
+    <input className='inputData' type="text" value={updatedata} 
+    onChange={getData}></input>
+    {console.log(data)}
+    <button className='displayBtn' onClick={addData}>{isEditing ? 'edit' : 'submit'}</button>
+    </span>
+    
+    {data.length > 0 && (
+    <span className='displayData'>{data.map((ele)=>{
+    return(
+    
+      < span className='listData' key={ele.id} ><p>{ele.content}</p>
+     < span className='btnData'> <button
+                type='button'
+                className='editBtn'
+                onClick={() => editItem(ele.id)}
+              ><FaEdit/></button>   
+              <button
+                type='button'
+                className='deleteBtn'
+                onClick={() => removeItem(ele.id)}
+              >
+                <FaTrash />
+              </button>
+              </span>
+              </span>
+    )}
+    )}
+     
+      <button className='clearBtn' onClick={clearList}>
+    clear items
+  </button> 
+   </span>
+    
+
+    )
+     
+    
+   
+}
     </div>
+    
+    </>
   );
+  
 }
 
 export default App;
